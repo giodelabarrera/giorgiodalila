301955a7bcd06aaee33f8883fc20a80eb4f0bfe9
giorgio <giorgio@onetechteam.com>
2015-04-14 09:23:29 +0200

    adaptacion de html template, no wp template. Pagina home con assets

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -1,73 +1,59 @@
-(function($){
-	
-	"use strict";
-	
-    $(document).ready(function(){
-
-		/* Hero height
-		================================================== */
-		var windowHeight = $(window).height();
-		
-		$('.hero').height( windowHeight );
-		
-		$(window).resize(function() {
-			
-			var windowHeight = $(window).height();
-			$('.hero').height( windowHeight );
-			
-		});
-
-		// Menu settings
-		$('#menuToggle, .menu-close').on('click', function(){
-			$('#menuToggle').toggleClass('active');
-			// $('body').toggleClass('body-push-toleft');
-			$('#theMenu').toggleClass('menu-open');
-		});
-
-		if($('#photostack').length > 0){
-			/* Gallery
-			================================================== */
-			new Photostack( document.getElementById( 'photostack' ), {
-				callback : function( item ) {
-					//console.log(item)
-				}
-			} );	
-				
-			/* Gallery popup
-			=================================================== */	
-			$('.photostack').magnificPopup({
-				delegate: 'a',
-				type: 'image',
-				tLoading: 'Loading image #%curr%...',
-				mainClass: 'mfp-img-mobile',
-				gallery: {
-					enabled: true,
-					navigateByImgClick: true,
-					preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-				},
-				image: {
-					tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-					titleSrc: function(item) {
-						return item.el.attr('title');
-					}
-				},
-				/* zoom: {
-					enabled: true,
-					duration: 300 // don't foget to change the duration also in CSS
-				} */
-			});
-		}	
-		if($('.blog-post-slider').length > 0){
-			$('.blog-post-slider').flexslider({
-	            animation: "fade",
-	            controlNav: false,
-	            slideshowSpeed: 4000
-
-	        });
-		}
-		
-		$('#commentform .form-submit #submit').addClass('btn btn-primary');	
-	});		
-
-
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        jQuery('#countdown_dashboard').countDown({
+            targetDate: {
+                'day': 7,
+                'month': 5,
+                'year': 2015,
+                'hour': 0,
+                'min': 0,
+                'sec': 0
+            }
+        });
+        var windowHeight = $(window).height();
+        $('.hero').height(windowHeight);
+        $(window).resize(function () {
+            var windowHeight = $(window).height();
+            $('.hero').height(windowHeight);
+        });
+        $('#menuToggle, .menu-close').on('click', function () {
+            $('#menuToggle').toggleClass('active');
+            $('body').toggleClass('body-push-toleft');
+            $('#theMenu').toggleClass('menu-open');
+        });
+        new Photostack(document.getElementById('photostack'), {
+            callback: function (item) {}
+        });
+        $('.photostack').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1]
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function (item) {
+                    return item.el.attr('title');
+                }
+            },
+        });
+        // jQuery.supersized({
+        //     slide_interval: 3000,
+        //     transition: 1,
+        //     transition_speed: 700,
+        //     slide_links: 'blank',
+        //     slides: [{
+        //         image: 'images/slider-1.jpg'
+        //     }, {
+        //         image: 'images/slider-2.jpg'
+        //     }, {
+        //         image: 'images/slider-3.jpg'
+        //     }]
+        // });
+    });
 })(jQuery);
